9f6dc255800f7186086e35d95f48561f4c6d9918
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-11-08 22:13:49 +0300

    Change latitude, longitude and zoom level.

--- app/src/main/java/com/mobile/geoapp/MapsActivity.java
+++ app/src/main/java/com/mobile/geoapp/MapsActivity.java
@@ -38,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng london = new LatLng(51.5123175, -0.1276609);
+
+        mMap.addMarker(new MarkerOptions().position(london).title("Marker in London"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(london, 15));
     }
 }
